ad8bbb2aa7e56f0916d79cebdcb4a0605c0afd84
crlne <carol.lages@outlook.com>
2021-03-18 15:02:31 -0300

    integration api

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 
@@ -8,6 +9,7 @@ export function Dashboard() {
     return (
         <Container>
             <Summary />
+            <TransactionsTable />
 
         </Container>
 
