77a01b556df234cd66bbb957f3edc8eb3954c3a0
crlne <carol.lages@outlook.com>
2021-03-19 01:36:19 -0300

    modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,59 +1,84 @@
 import { createGlobalStyle } from 'styled-components'
 
-export const GlobalStyle = createGlobalStyle `
-:root {
-    --background: #f0f2f5;
-    --red: #E52E4D;
-    --blue: #5429CC;
-    --green: #33CC95;
-
-    --blue-light: #6933FF;
-
-    --text-title: #363F5F;
-    --text-body: #969CB3;
-    --shape: #FFFFFF;
-}
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-html { //CONFIG. TODAS AS APLICAÇÕES
-    @media (max-width: 1080px) {
-        font-size: 93.75%;
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --background: #f0f2f5;
+        --red: #E52E4D;
+        --blue: #5429CC;
+        --green: #33CC95;
+        
+        --blue-light: #6933FF;
+        
+        --text-title: #363F5F;
+        --text-body: #969CB3;
+        --shape: #FFFFFF;
     }
-//se a tela do usuario for menor que
-    @media (max-width: 720px) { 
-        font-size: 87.5%;
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
     }
-}
 
-body {
-    background: var(--background);
-    -webkit-font-smoothing: antialiased;
-}
+    html { //CONFIG. TODAS AS APLICAÇÕES
+        @media (max-width: 1080px) {
+            font-size: 93.75%;
+        }
+        //se a tela do usuario for menor que
+        @media (max-width: 720px) { 
+            font-size: 87.5%;
+        }
+    }
 
-input, body, textarea, button {
-    font-family: 'Poppins', sans-serif;
-    font-weight: 400;
-}
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
 
-h1, h2, h3, h4, h5, h6, strong {
-    font-weight: 600;
-}
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
 
-h1 {
-    color: #8257e6;
-}
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
 
-button {
-    cursor: pointer;
-}
+    h1 {
+        color: #8257e6;
+    }
 
-[disabled] { 
-    opacity: 0.6;
-    cursor: not-allowed; //mostra quando não é permitido clicar
-}
+    button {
+        cursor: pointer;
+    }
 
-`
+    [disabled] { 
+        opacity: 0.6;
+        cursor: not-allowed; //mostra quando não é permitido clicar
+    }
+
+
+    .react-modal-overlay {
+        background: rgba(0, 0, 0, 0.5);
+        
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
+    }
+
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.24rem;   
+    }
+`;
